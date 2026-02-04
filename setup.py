5f07f4d0ee1833a7cee62e97668ce50e24c9f83f
CiRA-AMI <30405116+CiRA-AMI@users.noreply.github.com>
2026-02-04 12:56:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+## Updated: Support both catkin and pip install
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['ros_numpy'],
-    package_dir={'': 'src'})
+# Try to use catkin_pkg if available (for ROS environment)
+try:
+    from catkin_pkg.python_setup import generate_distutils_setup
+    
+    # fetch values from package.xml
+    setup_args = generate_distutils_setup(
+        packages=['ros_numpy'],
+        package_dir={'': 'src'})
+    
+    setup(**setup_args)
 
-setup(**setup_args)
+except ImportError:
+    # Fallback for pip install (non-ROS environment)
+    setup(
+        name='ros_numpy',
+        version='0.0.5',
+        description='Tools for converting ROS messages to and from numpy arrays',
+        author='Eric Wieser',
+        author_email='wieser.eric@gmail.com',
+        url='https://github.com/CiRA-AMI/cira_ros_numpy',
+        packages=find_packages(where='src'),
+        package_dir={'': 'src'},
+        install_requires=[
+            'numpy',
+        ],
+        python_requires='>=3.6',
+    )
